ffdff00ff8540350f6ad3e00f86329f446336bd6
Duguce <zhgyqc@163.com>
2022-03-26 17:25:41 +0800

    update Programming-Collective-Intelligence chapter03

--- Programming-Collective-Intelligence/chapter03/clusters.py
+++ Programming-Collective-Intelligence/chapter03/clusters.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from math import sqrt
 from PIL import Image, ImageDraw
+import random
 
 
 def readfile(filename):
@@ -78,6 +79,46 @@ def hcluster(rows, distance=pearson):
     return clust[0]
 
 
+def kcluster(rows, distance=pearson, k=4):
+    """K-均值聚类算法"""
+    # 确定每个点的最小值和最大值
+    ranges = [
+        (min(row[i] for row in rows), max(row[i] for row in rows))
+        for i in range(len(rows[0]))
+    ]
+    # 随机创建k个中心点
+    clusters = [[
+        random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+        for i in range(len(rows[0]))
+    ] for j in range(k)]
+    lastmatches = None
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches = [[] for i in range(k)]
+        # 在每一行中寻找距离最近的中心点
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row): bestmatch = i
+            bestmatches[bestmatch].append(j)
+        # 如果结果与上一次相同，则整个过程结束
+        if bestmatches == lastmatches: break
+        lastmatches = bestmatches
+        # 把中心点移到其所有成员的平均位置处
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+    return bestmatches
+
+
 def printclust(clust, labels=None, n=0):
     """递归遍历聚类数，并将其以类似文件系统层级结构的形式打印出来"""
     # 利用缩进来建立层级布局
@@ -152,11 +193,25 @@ def drawnode(draw, clust, x, y, scaling, labels):
         draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
 
 
+def rotatematrix(data):
+    newdata = []
+    for i in range(len(data[0])):
+        newrow = [data[j][i] for j in range(len(data))]
+        newdata.append(newrow)
+    return newdata
+
+
 def main():
     blognames, words, data = readfile('blogdata.txt')
-    clust = hcluster(data)
-    printclust(clust, labels=blognames)
-    drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+    # clust = hcluster(data)
+    # printclust(clust, labels=blognames)
+    # drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+    # rdata = rotatematrix(data)
+    # wordclust = hcluster(rdata)
+    # drawdendrogram(wordclust, labels=words, jpeg='wordclust.jpg')
+    kclust = kcluster(data, k=10)
+    print([blognames[r] for r in kclust[0]])
+    print([blognames[r] for r in kclust[1]])
 
 
 if __name__ == '__main__':
