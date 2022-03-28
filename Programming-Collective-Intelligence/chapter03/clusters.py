010563abd0f2959fc230cc7aba73092f1e12cf75
Duguce <zhgyqc@163.com>
2022-03-28 18:26:31 +0800

    update Programming-Collective-Intelligence chapter03

--- Programming-Collective-Intelligence/chapter03/clusters.py
+++ Programming-Collective-Intelligence/chapter03/clusters.py
@@ -36,6 +36,17 @@ def pearson(v1, v2):
     return 1.0 - num / den
 
 
+def tanimoto(v1, v2):
+    """Tanimoto计算距离"""
+    c1, c2, shr = 0, 0, 0
+    for i in range(len(v1)):
+        if v1[i] != 0: c1 += 1  # 出现在v1中
+        if v2[i] != 0: c2 += 1  # 出现在v2中
+        if v1[i] != 0 and v2[i] != 0: shr += 1  # 在两个向量中都出现
+
+    return 1.0 - (float(shr) / (c1 + c2 - shr + 1))  # 在两个向量中都出现
+
+
 class bicluster:
     """聚类"""
 
@@ -201,6 +212,56 @@ def rotatematrix(data):
     return newdata
 
 
+def scaledown(data, distance=pearson, rate=0.01):
+    """计算各项之间的距离"""
+    n = len(data)
+    # 每一对数据项之间的真实距离
+    realdist = [[distance(data[i], data[j]) for j in range(n)] for i in range(n)]
+    outersum = 0.0
+    # 随机初始化节点在二维空间中的起始位置
+    loc = [[random.random(), random.random()] for i in range(n)]
+    fakedist = [[0.0 for j in range(n)] for i in range(n)]
+    lasterror = None
+    for m in range(0, 1000):
+        # 寻找投影后的距离
+        for i in range(n):
+            for j in range(n):
+                fakedist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2) for x in range(len(loc[i]))]))
+        # 移动节点
+        grad = [[0.0, 0.0] for i in range(n)]
+        totalerror = 0
+        for k in range(n):
+            for j in range(n):
+                if j == k: continue
+                # 误差值等于目标距离与当前距离之间差值的百分比
+                errorterm = (fakedist[j][k] - realdist[j][k]) / realdist[j][k]
+                # 每一个节点都需要根据误差的多少按照比例移离或移向其他节点
+                grad[k][0] += ((loc[k][0] - realdist[j][0]) / fakedist[j][k]) * errorterm
+                grad[k][1] += ((loc[k][1] - realdist[j][0]) / fakedist[j][k]) * errorterm
+                # 记录总的误差值
+                totalerror += abs(errorterm)
+        print(totalerror)
+        # 如果节点移动之后的情况变得更糟，则程序结束
+        if lasterror and lasterror < totalerror: break
+        lasterror = totalerror
+        # 根据rate参数与grad值相乘的结果，移动每一个节点
+        for k in range(n):
+            loc[k][0] -= rate * grad[k][0]
+            loc[k][1] -= rate * grad[k][1]
+    return loc
+
+
+def draw2d(data, labels, jpeg='mds2d.jpg'):
+    """根据坐标值，在二维图上展示坐标的位置及其对应的标签"""
+    img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    for i in range(len(data)):
+        x = (data[i][0] + 0.5) * 1000
+        y = (data[i][1] + 0.5) * 1000
+        draw.text((x, y), labels[i], (0, 0, 0))
+    img.save(jpeg, 'JPEG')
+
+
 def main():
     blognames, words, data = readfile('blogdata.txt')
     # clust = hcluster(data)
@@ -209,9 +270,14 @@ def main():
     # rdata = rotatematrix(data)
     # wordclust = hcluster(rdata)
     # drawdendrogram(wordclust, labels=words, jpeg='wordclust.jpg')
-    kclust = kcluster(data, k=10)
-    print([blognames[r] for r in kclust[0]])
-    print([blognames[r] for r in kclust[1]])
+    # kclust = kcluster(data, k=10)
+    # print([blognames[r] for r in kclust[0]])
+    # print([blognames[r] for r in kclust[1]])
+    # wants, people, data = readfile('zebo.txt')
+    # clust = hcluster(data, distance=tanimoto)
+    # drawdendrogram(clust, wants)
+    coords = scaledown(data)
+    draw2d(coords, blognames, jpeg='blogs2d.jpg')
 
 
 if __name__ == '__main__':
